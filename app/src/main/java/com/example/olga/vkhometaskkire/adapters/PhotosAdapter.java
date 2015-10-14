d286ad4b7538cae3f927505643617d7d9b22a8df
pelipetsaa <pelipetsaa@gmail.com>
2015-10-14 22:24:15 +0300

    start

---
+++ app/src/main/java/com/example/olga/vkhometaskkire/adapters/PhotosAdapter.java
@@ -0,0 +1,60 @@
+package com.example.olga.vkhometaskkire.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.olga.vkhometaskkire.R;
+import com.example.olga.vkhometaskkire.datas.UtilsVK;
+
+import java.util.List;
+
+/**
+ * Created by Olga on 14.10.2015.
+ */
+public class PhotosAdapter extends ArrayAdapter<String> {
+    private String[] list;
+    private Context ctx;
+
+
+    public PhotosAdapter(Context context, int resource, String[] objects) {
+        super(context, resource, objects);
+        list = objects;
+        ctx = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //if (position)
+        ImageView view= null;
+        if (convertView==null){
+          view=new  ImageView(ctx);
+        }else{
+            view=(ImageView)convertView;
+        }
+        view.setImageBitmap(UtilsVK.getBitmapFromAssets(ctx, list[position]));
+        view.setPadding(10, 10, 10, 10);
+        view.setLayoutParams(new Gallery.LayoutParams(150, 150));
+        view.setScaleType(ImageView.ScaleType.FIT_XY);
+        view.setBackgroundResource(R.color.color_background_gray);
+        return view;
+        /*
+
+        View itemView = convertView;
+        if (itemView == null) {
+            itemView = View.inflate(getContext(), R.layout.item_photo_horizontal_gridview, null);
+        }
+        ImageView icon = (ImageView) itemView.findViewById(R.id.iv_item);
+
+        String pathIcon = list[position];
+
+        icon.setImageBitmap(UtilsVK.getBitmapFromAssets(ctx, pathIcon));
+
+
+        return itemView;*/
+    }
+}
--- app/src/main/java/com/example/olga/vkhometaskkire/datas/UtilsVK.java
+++ app/src/main/java/com/example/olga/vkhometaskkire/datas/UtilsVK.java
@@ -23,16 +23,31 @@ public class UtilsVK {
 
     public static void setList() {
        list=new ArrayList<User>();
-        list.add(new User(User.SEX_WOMAN,"Olga", "Pelipets","Olyalya", "Kharkov", "Ukraine", new String[]{"avatar1.jpg", "photo1.jpg", "photo2.jpg", "photo3.jpg", "photo4.jpg"}, null, "undefinied status", null, true,true, true, true, true, 0, 0, new int[]{2,3}));
+        list.add(new User(User.SEX_WOMAN,"Olga", "Pelipets","Olyalya", "Kharkov", "Ukraine", new String[]{"avatar1.jpg", "photo1.jpg", "photo2.jpg", "photo3.jpg", "photo4.jpg"}, null, "В активном поиске счастья жизни", null, true,true, true, true, true, 0, 0, new int[]{2,3}));
 
     }
 
     private static ArrayList<User> list;
 
-    public static Bitmap getBitmapFromAssets(Context ctx,String fileName) throws IOException{
+  /*  public static Bitmap getBitmapFromAssets(Context ctx,String fileName) throws IOException{
         AssetManager assetManager = ctx.getResources().getAssets();
         InputStream istr = assetManager.open(fileName);
         Bitmap bitmap = BitmapFactory.decodeStream(istr);
+        return bitmap;
+    }*/
+
+    public static Bitmap getBitmapFromAssets(Context ctx,String strName)
+    {
+        AssetManager assetManager = ctx.getAssets();
+        InputStream istr = null;
+        Bitmap bitmap=null;
+        try {
+            istr = assetManager.open(strName);
+            bitmap = BitmapFactory.decodeStream(istr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return bitmap;
     }
 
